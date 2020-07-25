e12c4c8cc68ea60a200d9c54c29daa8b78ba12ff
mickmed <mick2090@gmail.com>
2020-07-24 21:46:33 -0400

    adjust footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,18 +2,24 @@ import React from "react"
 
 import "../Header/Header.scss"
 import "./footer.scss"
-import { Link } from "react-router-dom"
 
 // import axios from "axios";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>
-        Website by <Link to='https://mick-roth.surge.sh'>Mick Roth &copy; </Link>2019
-        Gihub<Link to='https://mick-roth.surge.sh'>Mick Roth &copy; </Link>2019
-
-      </p>
+      <div>
+        Website by{" "}
+        <a target="_blank" href="https://mick-roth.surge.sh">
+          Mick Roth&copy;
+        </a>
+      </div>
+      <div>
+        Github{" "}
+        <a target="_blank" href="https://github.com/mickmed">
+          Github.com/mickmed
+        </a>
+      </div>
     </footer>
   )
 }
