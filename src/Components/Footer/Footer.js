fbbfb634771f7bc0a672acce9f1a4ecf70295291
mickmed <mick2090@gmail.com>
2020-07-24 21:33:04 -0400

    update readme

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,20 +1,21 @@
 import React from "react"
 
 import "../Header/Header.scss"
-import './footer.scss'
+import "./footer.scss"
+import { Link } from "react-router-dom"
 
 // import axios from "axios";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <p>Website by Mick Roth &copy; 2019</p>
-        </footer>
+  return (
+    <footer className="footer">
+      <p>
+        Website by <Link to='https://mick-roth.surge.sh'>Mick Roth &copy; </Link>2019
+        Gihub<Link to='https://mick-roth.surge.sh'>Mick Roth &copy; </Link>2019
 
-
-
-    )
-
+      </p>
+    </footer>
+  )
 }
 
 export default Footer
